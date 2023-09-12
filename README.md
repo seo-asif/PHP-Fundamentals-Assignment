f9cf8614e1768f2640e0d974fbafe23ccf27db8f
ASIF064 <asif064@gmail.com>
2023-09-12 22:11:31 +0600

    Add personal Info

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PHP-Fundamentals-Assignment
+# PHP-Fundamentals-Assignment-1
 
 ##Task 1: Personal Information Page
 Create a PHP file named personal_info.php that displays your personal information using variables and the echo statement. Include your name, age, country, and a brief introduction.
---
+++ temperature converter/temperature_converter.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Temperature Conveter</title>
+    <style>
+        <?php include "styles.css"?>
+    </style>
+</head>
+<body>
+<div class="container">
+    <h2>Temperature Conveter</h2>
+    <form>
+        <input type="text">
+    </form>
+
+</div>
+</body>
+</html>
